15709c64390362a6ca11e57bb9cd73b2fd8a6f2d
nick3point5 <ktt4444@gmail.com>
2023-12-03 16:01:43 -0500

    failing

---
+++ 1/multiply.js
@@ -0,0 +1,10 @@
+/**
+ * Multiplies two numbers.
+ *
+ * @param {number} x - The first number to multiply.
+ * @param {number} y - The second number to multiply.
+ * @return {number} The product of the two numbers.
+ */
+export function multiply(x, y) {
+	
+}
