dd9542c249a4fd1ca01f1c54aa667116645bfed9
nick3point5 <ktt4444@gmail.com>
2023-12-04 16:28:00 -0500

    rounding problem

--- problems/4_nodes_list/3/doublyLinkedList/doublyLinkedList.test.js
+++ problems/4_nodes_list/3/doublyLinkedList/doublyLinkedList.test.js
@@ -6,16 +6,16 @@ Deno.test("doublyLinkedList 1", () => {
 })
 
 Deno.test("doublyLinkedList 2", () => {
-	assertEquals (
+	assertEquals(
 		doublyLinkedList([1]),
-		new Node(1)
+		new Node(1),
 	)
 })
 
 Deno.test("doublyLinkedList 3", () => {
-	const node1 =  new Node(1)
-	const node2 =  new Node(2)
-	const node3 =  new Node(3)
+	const node1 = new Node(1)
+	const node2 = new Node(2)
+	const node3 = new Node(3)
 
 	node1.next = node2
 	node2.prev = node1
