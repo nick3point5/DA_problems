df4053065663c24ef4e909dc362b67019ec290ec
nick3point5 <ktt4444@gmail.com>
2024-01-18 16:35:24 -0500

    linked list problems added

---
+++ problems/7_linked_list/appendLists/appendLists.js
@@ -0,0 +1,17 @@
+import { Node } from "../../4_nodes_list/2/linkedList/linkedList.js"
+
+/**
+ * Appends two linked lists together.
+ *
+ * @param {Node} list1 - The first linked list.
+ * @param {Node} list2 - The second linked list.
+ * @return {Node} The appended linked list.
+ * @example
+ * appendLists(
+ *   [1, 2, 3],
+ *   [4, 5, 6]
+ * ) => 1 -> 2 -> 3 -> 4 -> 5 -> 6
+ */
+export function appendLists(list1, list2) {
+	return null
+}
---
+++ problems/7_linked_list/findLength/findLength.js
@@ -0,0 +1,11 @@
+/**
+ * Calculates the length of a linked list.
+ *
+ * @param {Node} head - The head of the linked list.
+ * @return {number} The length of the linked list.
+ * @example
+ * findLength([1, 2, 3, 4, 5]) => 5
+ */
+export function findLength() {
+	return null
+}
