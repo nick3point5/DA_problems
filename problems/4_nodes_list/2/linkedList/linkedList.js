ee9f06e6e33e7a2124a2afbfca08f8d85ad8b7df
DomineersXII <channen1titan@gmail.com>
2024-02-27 19:24:37 -0500

    s

--- problems/4_nodes_list/2/linkedList/linkedList.js
+++ problems/4_nodes_list/2/linkedList/linkedList.js
@@ -25,17 +25,20 @@ export class Node {
  * @return {Node} - The head of the linked list.
  */
 export function linkedList(array) {
-	let previousNode = null
-	let nodeHead = null
+	if (array.length === 0) {
+		return null
+	}
+
+	let head = new Node(null)
+	let currentNode = head
+
 	for (let i = 0; i < array.length; i++) {
 		const newNode = new Node(array[i])
-		if (nodeHead === null) {
-			nodeHead = newNode
-		}
-		if (previousNode !== null) {
-			previousNode.next = newNode
-		}
-		previousNode = newNode
+		currentNode.next = newNode
+		currentNode = newNode
 	}
-	return nodeHead
+
+	return head.next
 }
+
+linkedList([1, 2, 3])
