f7187bee9cc8b209eb256f8bd57096fb749d6f80
DomineersXII <channen1titan@gmail.com>
2024-03-04 17:00:10 -0500

    s

--- problems/1_find_maximum_value/4/findMaxND/findMaxND.js
+++ problems/1_find_maximum_value/4/findMaxND/findMaxND.js
@@ -4,6 +4,15 @@
  * @param {Array} array - The n-dimensional array to search.
  * @return {number} - Returns the maximum value in the array. Returns null if the array is empty..
  */
-export function findMaxND(array) {
-	return null
+export function findMaxND(arr) {
+	let flat = arr.flat()
+	if (flat.length === 0) {
+		return null
+
+	} else {
+		return Math.max(...flat)
+
+
+	}
 }
+
