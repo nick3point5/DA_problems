80e30ba1f8e6df0963822973dc2502bd83c26c93
DomineersXII <channen1titan@gmail.com>
2024-03-10 11:44:19 -0400

    s

--- problems/9_trees/3/makeTree/makeTree.ts
+++ problems/9_trees/3/makeTree/makeTree.ts
@@ -36,9 +36,22 @@ export function makeTree(array: NestedArray) {
 		return null
 	}
 	
-	for (let i = 0; i < array.length; i++) {
-		
-	}
-}
+	//split array into base and then the branches for example [1, [null, null]] will be turned into [1] [null,null]
+	const [base, branches] = array
+	//make node of the base
+	const node = new TreeNode(base)
 
-makeTree([1, [[2, [null, null]], [3, [null, null]]]])
+	//check if there is a left or right node if there is then call the makeTree function again 
+	if (branches[0]) {
+		//make left node then call the makeTree function again to check if there are any new branches connected to the left branch
+        node.left = makeTree(branches[0]);
+    }
+    if (branches[1]) {
+		//make right node then call the makeTree function again to check if there are any new branches connected to the right branch
+        node.right = makeTree(branches[1]);
+    }
+
+	//returns the base node after all the recursion is done
+	//if it is a recursion function then it will return the node that it created but the final return will be from function called first
+	return node
+}
