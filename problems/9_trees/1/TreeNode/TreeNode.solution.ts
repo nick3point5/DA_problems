cdf145b067cd506fe2e44993b41b9282b30d1e52
nick3point5 <ktt4444@gmail.com>
2024-03-11 17:17:50 -0400

    solution added

---
+++ problems/9_trees/1/TreeNode/TreeNode.solution.ts
@@ -0,0 +1,17 @@
+export class TreeNode<T> {
+	value: T
+	left: TreeNode<T> | null
+	right: TreeNode<T> | null
+	/**
+	 * Constructor for creating a new node with a given value and optional left and right children.
+	 *
+	 * @param {T} value - the value for the new node
+	 * @param {TreeNode<T> | null} left - the left child node (optional)
+	 * @param {TreeNode<T> | null} right - the right child node (optional)
+	 */
+	constructor(value: T, left: TreeNode<T> | null = null, right: TreeNode<T> | null = null) {
+		this.value = value
+		this.left = left
+		this.right = right
+	}
+}
