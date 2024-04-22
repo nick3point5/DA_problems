c90e5dd99b266077e65036c0cfe39aab961a537b
DomineersXII <channen1titan@gmail.com>
2024-04-22 17:11:55 -0400

    finished all map

--- problems/10_maps/10/morseDecode/morseDecode.ts
+++ problems/10_maps/10/morseDecode/morseDecode.ts
@@ -11,6 +11,16 @@ const morseMap: Map<string, string> = new Map(Object.entries(jsonData))
  * morseDecode("... --- ...") = "sos"
  * morseDecode(".... . .-.. .-.. --- / .-- --- .-. .-.. -..") = "hello world"
  */
-export function morseDecode() {
-	return null
+export function morseDecode(s: string): string {
+    const reversedMap = new Map<string,string>()
+
+    for (const [key, value] of morseMap) {
+        reversedMap.set(value, key)
+    }
+
+
+	const encodedWords = s.split(" / ") //split the sentence into seperate words
+    const decodedWords = encodedWords.map(word => word.split(" ").map(letter => reversedMap.get(letter)).join("")) //split the morse symbols of the word, set each symbol to a letter, join the letters together to make a word
+
+    return decodedWords.join(" ") //join all the words to the sentence
 }
--- problems/10_maps/8/romanNumerals/romanNumerals.ts
+++ problems/10_maps/8/romanNumerals/romanNumerals.ts
@@ -14,31 +14,32 @@
  * romanNumerals(1000) = "M"
  * romanNumerals(3999) = "MMMCMXCIX"
  */
-export function romanNumerals(num: number): string {
-	const romanNumerals = new Map<number,string>([
-		[1, "I"],
-		[5, "V"],
-		[10, "X"],
-		[50, "L"],
-		[100, "C"],
-		[500, "D"],
-		[1000, "M"]
-	])
-
-	const specialCases = [
-		["DCCCC", "CM"],
-		["CCCC", "CD"],
-		["LXXXX", "XC"],
-		["XXXX", "XL"],
-		["VIIII", "IX"],
-		["IIII", "IV"],
-	]
-
+export function romanNumerals(input: number): string {
+	const romanNumerals = [ //dictionary instead of map because it has an explicit order
+		{ number: 1000, numeral: "M" },
+		{ number: 900, numeral: "CM" },
+		{ number: 500, numeral: "D" },
+		{ number: 400, numeral: "CD" },
+		{ number: 100, numeral: "C" },
+		{ number: 90, numeral: "XC" },
+		{ number: 50, numeral: "L" },
+		{ number: 40, numeral: "XL" },
+		{ number: 10, numeral: "X" },
+		{ number: 9, numeral: "IX" },
+		{ number: 5, numeral: "V" },
+		{ number: 4, numeral: "IV" },
+		{ number: 1, numeral: "I" },
+	];
 
 	let result = ""
-	let subtractor = 1000
+
+	for (const {number, numeral} of romanNumerals) {
+		while (input >= number) {
+			result = result.concat(numeral)
+			input -= number
+		}
+	}
 	
-	
-	
+
 	return result
 }
