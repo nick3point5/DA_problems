0817009753bbd9c9c08266f3ef30bf757f2f0c84
nick3point5 <ktt4444@gmail.com>
2024-04-22 17:16:39 -0400

    searching 2 & 3 added

---
+++ problems/11_searching/3/findSorted/findSorted.ts
@@ -0,0 +1,10 @@
+/**
+ * Finds the index of the target element in a sorted array using binary search.
+ *
+ * @param {Array} array - The sorted array to search in.
+ * @param {any} target - The element to find in the array.
+ * @return {number} The index of the target element in the array, or -1 if it is not found.
+ */
+export function findSorted() {
+	return null
+}
