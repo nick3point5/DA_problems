9b2b257e6e1c1510bf704c33c2adf10fc6b107d6
DomineersXII <channen1titan@gmail.com>
2024-04-25 17:01:34 -0400

    binary sort

--- problems/11_searching/2/findUnsorted/findUnsorted.ts
+++ problems/11_searching/2/findUnsorted/findUnsorted.ts
@@ -5,6 +5,15 @@
  * @param {number} target - The target element to find.
  * @return {number} The index of the target element in the array, or -1 if not found.
  */
-export function findUnsorted() {
-	return null
+export function findUnsorted(array: number[], target: number): number {
+	let returnVal = -1
+
+	for (let i = 0; i < array.length; i++) {
+		if (array[i] === target) {
+			returnVal = i
+			break
+		}
+	}
+
+	return returnVal
 }
--- problems/11_searching/3/findSorted/findSorted.ts
+++ problems/11_searching/3/findSorted/findSorted.ts
@@ -5,6 +5,21 @@
  * @param {any} target - The element to find in the array.
  * @return {number} The index of the target element in the array, or -1 if it is not found.
  */
-export function findSorted() {
-	return null
+export function findSorted(array: number[], target: number): number {
+	let left = 0
+    let right = array.length - 1
+
+	while (left <= right) {
+        const middleIndex = Math.floor((left + right)/2)
+			
+        if (array[middleIndex] === target) {
+            return middleIndex
+        } else if (target < array[middleIndex]) {
+            right = middleIndex - 1
+        } else {
+            left = middleIndex + 1
+        }
+    }
+
+    return -1 //the element doesnt exist
 }
