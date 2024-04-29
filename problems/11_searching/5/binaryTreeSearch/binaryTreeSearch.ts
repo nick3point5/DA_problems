2e867b4156a0421624a821f61f86f652bcd0d683
nick3point5 <ktt4444@gmail.com>
2024-04-29 17:10:40 -0400

    json tree solution

--- problems/11_searching/5/binaryTreeSearch/binaryTreeSearch.ts
+++ problems/11_searching/5/binaryTreeSearch/binaryTreeSearch.ts
@@ -11,7 +11,7 @@ import { TreeNode } from "../../../9_trees/1/TreeNode/TreeNode.ts"
  * 	[1, [0, 2]],
  *  0
  * ) => 0
- * 
+ *
  *       1
  *      / \
  *     0   2
@@ -20,7 +20,7 @@ import { TreeNode } from "../../../9_trees/1/TreeNode/TreeNode.ts"
  * 	[1, [[0, [null, null], [3, [2, [null, null]], [5, [4, null, null], [6, null, null]]]]]],
  * 	5
  * ) => 5
- * 
+ *
  *       1
  *      / \
  *     0   3
@@ -33,7 +33,7 @@ import { TreeNode } from "../../../9_trees/1/TreeNode/TreeNode.ts"
  * 	[1, [[0, [null, null], [3, [2, [null, null]], [5, [4, null, null], [6, null, null]]]]]],
  * 	7
  * ) => null
- * 
+ *
  *       1
  *      / \
  *     0   3
