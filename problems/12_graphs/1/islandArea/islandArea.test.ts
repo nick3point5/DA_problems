6f634a755d41c014eb1e8830011aed891d05356a
nick3point5 <ktt4444@gmail.com>
2024-05-02 17:16:22 -0400

    fix graph tests

--- problems/12_graphs/1/islandArea/islandArea.test.ts
+++ problems/12_graphs/1/islandArea/islandArea.test.ts
@@ -1,6 +1,56 @@
-import { islandArea } from "./islandArea.solution.ts"
+import { islandArea } from "./islandArea.ts"
 import { assertEquals } from "https://deno.land/std@0.208.0/assert/mod.ts"
 
 Deno.test("islandArea 1", () => {
-	assertEquals(islandArea(1), null)
+	const map1 = [
+		[5],
+	]
+
+	const result = islandArea(map1)
+	const expected = 1
+
+	assertEquals(result, expected)
+})
+
+Deno.test("islandArea 2", () => {
+	const map2 = [
+		[0, 0, 0, 0, 0],
+		[0, 0, 1, 0, 0],
+		[0, 0, 1, 0, 0],
+		[0, 0, 1, 0, 0],
+		[0, 0, 0, 0, 0],
+	]
+
+	const result = islandArea(map2)
+	const expected = 3
+
+	assertEquals(result, expected)
+})
+
+Deno.test("islandArea 3", () => {
+	const map3 = [
+		[4, 1, 0, 1, 1],
+		[1, 1, 2, 1, 0],
+		[0, 0, 1, 0, 0],
+		[0, 0, 1, 1, 2],
+	]
+
+	const result = islandArea(map3)
+	const expected = 12
+
+	assertEquals(result, expected)
+})
+
+Deno.test("islandArea 4", () => {
+	const map4 = [
+		[0, 0, 0, 0, 0],
+		[0, 2, 1, 0, 0],
+		[0, 0, 1, 0, 0],
+		[0, 0, 1, 3, 1],
+	]
+
+	const result = islandArea(map4)
+	const expected = 6
+
+	assertEquals(result, expected)
 })
--- problems/12_graphs/2/islandPerimeter/islandPerimeter.test.ts
+++ problems/12_graphs/2/islandPerimeter/islandPerimeter.test.ts
@@ -1,4 +1,4 @@
-import { islandPerimeter3 as islandPerimeter } from "./islandPerimeter.solution.ts"
+import { islandPerimeter3 as islandPerimeter } from "./islandPerimeter.ts"
 import { assertEquals } from "https://deno.land/std@0.208.0/assert/mod.ts"
 
 Deno.test("islandPerimeter 1", () => {
