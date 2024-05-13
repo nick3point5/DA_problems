9fb64c7381d26d4ca2d7e7205ba9b6f5f5b19444
nick3point5 <ktt4444@gmail.com>
2024-05-13 17:09:54 -0400

    update graph node question

--- problems/12_graphs/5/GraphNode/GraphNode.ts
+++ problems/12_graphs/5/GraphNode/GraphNode.ts
@@ -2,12 +2,12 @@
  * GraphNode class with the specified value, neighbors, and visited state.
  *
  * @property {T} value - The value of the graph node.
- * @property {GraphNode<T>[]} - The neighbors of the graph node. Defaults to an empty array.
- * @property {boolean} - The visited state of the graph node. Defaults to false.
- * @property {number} - The distance of the graph node. Defaults to 0.
- * @property {GraphNode<T> | null} - The previous node in the shortest path. Defaults to null.
+ * @property {GraphNode<T>[]} neighbors - The neighbors of the graph node. Defaults to an empty array.
+ * @property {boolean} visited - The visited state of the graph node. Defaults to false.
+ * @property {number} distance - The distance of the graph node. Defaults to 0.
+ * @property {GraphNode<T> | null} previous - The previous node in the shortest path. Defaults to null.
  */
-export class graphNode {
+export class GraphNode {
 	constructor() {
 	}
 }
