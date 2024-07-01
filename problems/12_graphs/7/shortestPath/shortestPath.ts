7bc8c300fd72701ec7d013e0b73d023c1afa5b34
DomineersXII <channen1titan@gmail.com>
2024-07-01 17:28:57 -0400

    shortestpath progress

--- problems/12_graphs/7/shortestPath/shortestPath.ts
+++ problems/12_graphs/7/shortestPath/shortestPath.ts
@@ -7,6 +7,16 @@ import { GraphNode } from "../../5/GraphNode/GraphNode.ts"
  * @param {number} target - The target node value.
  * @return {GraphNode<number> | null} - The target node, which previous property represents the shortest path from the root to the target, or null if no path is found.
  */
-export function shortestPath() {
+export function shortestPath(root: GraphNode<number>, target: number): GraphNode<number> | null {
+
+	function breathFirst(node: GraphNode<number>, distance: number) {
+		node.visited = true
+		node.distance = distance
+
+		
+	}
+
+	shortestPath(root, 0)
+
 	return null
 }
