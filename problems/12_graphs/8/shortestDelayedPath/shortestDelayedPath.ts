a3c170e00b7f2ad2ec809d9fcfd3c884fa0d1965
nick3point5 <ktt4444@gmail.com>
2024-07-03 15:56:57 -0400

    shortest delay path explaination

--- problems/12_graphs/8/shortestDelayedPath/shortestDelayedPath.ts
+++ problems/12_graphs/8/shortestDelayedPath/shortestDelayedPath.ts
@@ -2,6 +2,16 @@ import { GraphNode } from "../../5/GraphNode/GraphNode.ts"
 
 /**
  * Finds the shortest delayed path from the root node to the target node in a graph.
+ * The node's value indicate the distance it takes to move through.
+ * Consider the following graph
+ * 
+ * start - 5 - end
+ * 	   |       |
+ * 		 1 - 1 - 1
+ * 
+ * The path going to through 5 takes longer than going through the 1's.
+ * So the path should favor going through the 1's rather than the 5.
+ * 
  *
  * @param {GraphNode<number>} root - The starting node of the path.
  * @param {GraphNode<number>} target - The target node to reach.
