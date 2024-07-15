92dc16378cc812ec8c4f1964872645912fa6fb51
DomineersXII <channen1titan@gmail.com>
2024-07-15 17:02:06 -0400

    finished checkerboard

--- problems/13_drawing/6/checkerBoard/checkerBoard.ts
+++ problems/13_drawing/6/checkerBoard/checkerBoard.ts
@@ -31,5 +31,19 @@
  * """
  */
 export function checkerBoard(height: number, width: number): string {
-	return null
+	let board = ""
+	let xoro = 0
+
+    for (let i = 0; i < height; i++) {
+		board += "#".repeat(width*3) + "\r\n#"
+
+        for (let j = 0; j < width; j++) {
+			board += (xoro%2===0 ? "X" : "O") + (j === width-1 ? "#" : "##")
+			
+			xoro++
+        }
+        board += "\r\n" + "#".repeat(width*3) + "\r\n"
+	}
+
+    return board
 }
